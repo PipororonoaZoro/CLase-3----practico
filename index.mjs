315b98180a80d731f339f03de5cf45c8c95d3e17
PipororonoaZoro <maciel_ponce_9989@outlook.com.ar>
2026-03-12 19:24:45 -0300

    corrijiendo cambios

--- index.mjs
+++ index.mjs
@@ -3,8 +3,8 @@ import { leerSuperheroes, agregarSuperheroes } from './utils.mjs';
 const archivoOriginal = './superheroes.txt';
 const archivoNuevo = './agregarSuperheroes.txt';
 
-// Agregar nuevos superhéroes
-agregarSuperheroes(archivoOriginal, archivoNuevo);
+// Agregar nuevos superhéroes (descomenta si necesitas agregar más)
+// agregarSuperheroes(archivoOriginal, archivoNuevo);
 
 // Leer y mostrar la lista actualizada
 const superheroes = leerSuperheroes(archivoOriginal);
